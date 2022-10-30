5c9e241ec36e7b99c6d00771dfcfd3871a485ad2
Faruqi Hafiz <faruqihafiz21@gmail.com>
2022-10-30 14:44:23 +0700

    feat/fix bug

--- MODUL 2 FARUQI/faruqi_booking.php
+++ MODUL 2 FARUQI/faruqi_booking.php
@@ -13,7 +13,7 @@ require "db.php";
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>ESD Venue</title>
+    <title>ESD CAR RENT</title>
 </head>
 
 <body>
